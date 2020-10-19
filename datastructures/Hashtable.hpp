881e06617605b259eaea61184f39125d72ee3496
JaydenElliott <jayden.elliott@outlook.com>
2020-10-20 10:05:51 +1100

    FIX: hash table deconstructor fix

--- datastructures/Hashtable.hpp
+++ datastructures/Hashtable.hpp
@@ -12,7 +12,6 @@ class HashNode {
         this->price = price;
         this->ID = ID;
     }
-    ~HashNode(){};
 
     float price;
     size_t ID;
@@ -28,80 +27,114 @@ class HashTable {
         }
         this->sizeExponent = sizeExponent;
         this->tableSize = pow(2, sizeExponent);
-        this->hashVector = vector<HashNode *>(tableSize, 0);
+        this->hashVector = vector<HashNode *>(tableSize, nullptr);
     }
-    ~HashTable() {
-        for (int i = 0; i < hashVector.size(); i++) {
-            delete hashVector.at(i);
-        }
-    };
+    // ~HashTable() {
+    //     for (int i = 0; i < hashVector.size(); i++) {
+    //         cout << "iter " << i << endl;
+    //         HashNode *currnode = hashVector.at(i);
 
-    size_t generateHash(size_t ID) {
-        return ((ID * knuth) >> (32 - this->sizeExponent)) % tableSize;  // TODO check
-    }
-
-    void put(int price, size_t ID) {
-        size_t newIndex = generateHash(ID);
-        if (hashVector.at(newIndex) == 0) {
-            hashVector.at(newIndex) = new HashNode(price, ID);
-        } else {
-            HashNode *currNode = hashVector.at(newIndex);
-            while (currNode->nextNode != nullptr) {
-                currNode = currNode->nextNode;
-            }
-            currNode->nextNode = new HashNode(price, ID);
-        }
-    }
-
-    bool get(size_t ID, HashNode &node) {
-        size_t newIndex = generateHash(ID);
-        HashNode *initElem = hashVector.at(newIndex);
-        while (initElem != nullptr) {
-            if (initElem->ID == ID) {
-                node = *initElem;
-                return true;
-            }
-            initElem = initElem->nextNode;
-        }
-        return false;
-    }
-
-    bool getPrev(size_t ID, HashNode &node) {
-        size_t newIndex = generateHash(ID);
-        HashNode *initElem = hashVector.at(newIndex);
-        HashNode *prevElem;
-        while (initElem != nullptr) {
-            if (initElem->ID == ID) {
-                node = *prevElem;
-                return true;
-            }
-            prevElem = initElem;
-            initElem = initElem->nextNode;
-        }
-        return false;
-    }
+    //         while (currnode != nullptr) {
+    //             HashNode *tempNode = currnode;
+    //             cout << "HERE " << currnode << endl;
+    //             currnode = currnode->nextNode;
+    //             delete tempNode;
+    //         }
+    //         hashVector.at(i) = nullptr;
+    //     }
+    //     cout << "end destruct" << endl;
+    // };
 
     int sizeExponent;
     int tableSize;
     vector<HashNode *> hashVector;
     bool del(size_t ID);
     void print();
+    bool getelem(size_t ID);
+    bool get(size_t ID, HashNode &node);
+    void put(int price, size_t ID);
+    size_t generateHash(size_t ID);
 };
 
-bool HashTable::del(size_t ID) {
-    HashNode *currNode;
-    HashNode *prevNode;
-    bool res = get(ID, *currNode);
-    bool prev = getPrev(ID, *prevNode);
-    if (prev == true) prevNode->nextNode = currNode->nextNode;
-    if (res == true) {
-        delete currNode;
+size_t HashTable::generateHash(size_t ID) {
+    return ((ID * knuth) >> (32 - this->sizeExponent)) % tableSize;  // TODO check
+}
+
+void HashTable::put(int price, size_t ID) {
+    size_t newIndex = generateHash(ID);
+    if (hashVector[newIndex] == nullptr) {
+        hashVector[newIndex] = new HashNode(price, ID);
+    } else {
+        HashNode *currNode = hashVector[newIndex];
+        while (currNode->nextNode != nullptr) {
+            currNode = currNode->nextNode;
+        }
+        currNode->nextNode = new HashNode(price, ID);
+    }
+}
+
+bool HashTable::get(size_t ID, HashNode &node) {
+    size_t newIndex = generateHash(ID);
+    HashNode *initElem = hashVector.at(newIndex);
+    cout << initElem->ID << endl;
+    while (initElem != nullptr) {
+        if (initElem->ID == ID) {
+            cout << "!! 1 !!" << endl;
+            node = *initElem;
+            cout << "!! 2 !!" << endl;
+            return true;
+        }
+        initElem = initElem->nextNode;
     }
     return false;
 }
 
+bool HashTable::del(size_t ID) {
+    size_t Index = generateHash(ID);
+    HashNode *currNode = hashVector[Index];
+    HashNode *prevNode = nullptr;
+
+    if (currNode == nullptr) {
+        cout << "Already deleted" << endl;
+        return false;
+    }
+
+    while (currNode != nullptr && currNode->ID != ID) {
+        prevNode = currNode;
+        currNode = currNode->nextNode;
+    }
+    if (currNode == nullptr) {
+        cout << "a;lskdjf;kl" << endl;
+        return false;
+    }
+    if (prevNode == nullptr) {
+        cout << "condit 1" << endl;
+        this->hashVector[Index] = currNode;
+        cout << "hashy" << hashVector[Index]->ID << endl;
+        cout << "currnode = " << currNode->ID << endl;
+        // cout << hashVector[Index] << endl;
+        // cout << currNode->ID << endl;
+
+    } else {
+        cout << "condit 2" << endl;
+        prevNode->nextNode = currNode;
+    }
+
+    cout << "hashy2 = " << hashVector[Index]->ID << endl;
+    cout << "currnode2 = " << currNode->ID << endl;
+    delete currNode;
+    currNode = nullptr;
+    cout << "currnode2 = " << currNode << endl;
+    cout << "hash vector urg" << hashVector[Index] << endl;
+    // cout << currNode->ID << endl;
+    return true;
+}
+
 void HashTable::print() {
     for (int i = 0; i < hashVector.size(); i++) {
+        if (hashVector.at(i) == nullptr) {
+            continue;
+        }
         cout << hashVector.at(i)->ID << endl;
         cout << "start linked list" << endl;
         if (hashVector.at(i)->nextNode != nullptr) {
--- datastructures/egmain.cpp
+++ datastructures/egmain.cpp
@@ -1,5 +1,8 @@
+
+
 #include "../importsGlobal.hpp"
 #include "Hashtable.hpp"
+#include "Node.hpp"
 #include "Rbtree.hpp"
 // #include "Rbtree.hpp"
 
@@ -7,20 +10,27 @@ using namespace std;
 
 int main() {
     //---------------HASHTABLE--------------//
-    // HashTable testTable;
-    // testTable = HashTable(0);
-    // testTable.put(30, 11211);
+    HashTable testTable;
+    testTable = HashTable(1);
+    testTable.put(30, 11211);
     // testTable.put(28, 11161);
     // testTable.put(22, 11811);
     // testTable.put(19, 11111);
-    // testTable.print();
-    // cout << "start delele" << endl;
-    // cout << testTable.del(11111) << endl;
-    // testTable.print();
-    // // cout << testTable.hashVector.at(0)->ID;
+    cout << "!!! start print 1 !!!" << endl;
+    testTable.print();
+    cout << "!!! end print 1 !!!" << endl;
+    cout << endl;
+    cout << "!!! start delele !!!" << endl;
+    bool a = testTable.del(11211);
+    cout << (a ? "true" : "false") << endl;
+    cout << "!!! finish delele !!!" << endl;
+    cout << endl;
+
+    cout << testTable.hashVector.at(1)->ID << endl;
 
     //---------------RBTREE--------------//
 
-    RBtree testing = RBtree("buy");
-    return 0;
+    // RBtree testing = RBtree("buy");
+
+    // testing.insert_price("") return 0;
 }
